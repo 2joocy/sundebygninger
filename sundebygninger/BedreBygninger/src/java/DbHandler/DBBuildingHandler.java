9a95d0bdd037a60e289a7ff7c9838c73d71228c3
2joocy <william.pfaffe@gmail.com>
2016-10-22 22:03:05 +0200

    Missing functionality / layout for building and service

--- sundebygninger/BedreBygninger/src/java/DbHandler/DBBuildingHandler.java
+++ sundebygninger/BedreBygninger/src/java/DbHandler/DBBuildingHandler.java
@@ -110,29 +110,80 @@ public class DBBuildingHandler {
                 + "  </table>";
         return tableData;
     }
+    
+        public String getService(int idUser) {
+        String tableData = "<table class='table table-hover'>\n"
+                + "    <thead>\n"
+                + "      <tr>\n"
+                + "        <th>Address</th>\n"
+                + "        <th>City</th>\n"
+                + "        <th>Building Year</th>\n"
+                + "      </tr>\n"
+                + "    </thead>\n"
+                + "    <tbody>";
+        try {
+            /*
+    int idBuilding, String address, String cadastral, String buildingGrade, 
+    String area, String zipcode, String city, String condition, String service, 
+    String extraText, double buildingArea, int builtYear, int fk_idUser, 
+    int fk_MainPicture, int fk_idReport, Date dateCreated
+             */
+
+            Connection myConn = DBConnection.getConnection();
+            String sql = "SELECT idBuilding, address, cadastral, builtYear, area, zipcode, city, condition, service, extraText, dateCreated, fk_idUser, fk_idMainPicture, fk_idReport FROM building WHERE fk_idUser=?";
+            PreparedStatement prepared = myConn.prepareStatement(sql);
+            prepared.setInt(1, idUser);
+            ResultSet myRS = prepared.executeQuery();
+            while (myRS.next()) {
+                int idBuilding = myRS.getInt("idBuilding");
+                String address = myRS.getString("address");
+                String city = myRS.getString("city");
+                int builtYear = myRS.getInt("builtYear");
+
+                tableData += "<tr><form method ='POST' action='Front'><td>" + address + "</td><td>" + city + "</td><td>" + builtYear + "<input type='hidden' name='methodForm' value='serviceBuilding'><input type='hidden' name='idBuilding' value='" + idBuilding + "'></td></tr>";
+
+//                Building building = new Building(myRS.getInt("idBuilding"), 
+//                                                 myRS.getString("address"),
+//                                                 myRS.getString("cadastral"),
+//                                                 myRS.getString("buildingGrade"),
+//                                                 myRS.getString("area"),
+//                                                 myRS.getString("zipcode"),
+//                                                 myRS.getString("city"),
+//                                                 myRS.getString("condition"),
+//                                                 myRS.getString("service"),
+//                                                 myRS.getString("extraText"),
+//                                                 myRS.getDouble("buildingArea"),
+//                                                 myRS.getInt("builtYear"),
+//                                                 myRS.getInt("fk_idUser"),
+//                                                 myRS.getInt("fk_MainPicture"),
+//                                                 myRS.getInt("fk_idReport"),
+//                                                 myRS.getDate("dateCreated")
+//                                                );
+//                buildingList.add(building);
+            }
+        } catch (SQLException | HeadlessException ex) {
+
+        }
+        tableData += "</tbody>\n"
+                + "  </table>";
+        return tableData;
+    }
 
     public String createMenu(String status) {
+        DBUserHandler db = new DBUserHandler();
         String menu = "";
         if (status.equalsIgnoreCase("customer")) {
-
-            return "<div id='mySidenav' class='sidenav'>"
-                    + "<a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>&times;</a>"
-                    + "<a href='overviewBuilding.jsp'>Estates</a>"
-                    + "<a href='service.jsp'>Apply Service</a>"
-                    + "<a href='orderHistory.jsp'>Order History</a>"
-                    + "<a href='overviewAccount.jsp'>Account Management</a>"
-                    + "<a href='contact.jsp'>Help</a>"
-                    + "</div>";
+            return "<li><a href='overviewBuilding.jsp'>Estate</a></li>\n" +
+"            <li><a href='service.jsp'>Service Overview</a></li>\n" +
+"            <li><a href='account.jsp'>Account Management</a></li>\n" +
+"            <li><a href='contact.jsp'>Contact Staff</a></li>";
         } else if (status.equalsIgnoreCase("worker")) {
-
-            return "<div id='mySidenav' class='sidenav'>%>"
-                    + "<a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>&times;</a>"
-                    + "<a href='overviewBuilding.jsp'>Estates</a>"
-                    + "<a href='overviewUsers.jsp'>Account Management(<% out.print(db.countUnConfirmed());%>)</a>"
-                    + "<a href='orderHistory.jsp'>Order History</a>"
-                    + "<a href='overviewAccount.jsp'>Account Management</a>"
-                    + "<a href='contact.jsp'>Help</a>"
-                    + "</div>";
+            return "<li><a href='overviewBuilding.jsp'>Estates</a></li>\n" +
+"            <li><a href='service.jsp'>Service Overview</a></li>\n" +
+"            <li><a href='overviewUsers.jsp'>Account Management("+ db.countUnConfirmed() + ")</a></li>\n" +
+"            <li><a href='contact.jsp'>Contact Staff</a></li>";
+        }else if(status == null){
+            return "Unspecified Login. Please retry!";
         }
         return "";
     }
