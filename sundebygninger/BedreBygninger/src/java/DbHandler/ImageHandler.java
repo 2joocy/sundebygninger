a09e9b7fbdd0df23e0547aa36eace4b6da9ebfb5
vikto <vikto@DESKTOP-3G4A7F5>
2016-11-18 13:59:28 +0100

    Started work on thumbnail

--- sundebygninger/BedreBygninger/src/java/DbHandler/ImageHandler.java
+++ sundebygninger/BedreBygninger/src/java/DbHandler/ImageHandler.java
@@ -1,14 +1,22 @@
 package DbHandler;
 
+import java.awt.Graphics2D;
 import java.awt.HeadlessException;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.imageio.ImageIO;
 import javax.servlet.http.Part;
+import javax.swing.JOptionPane;
 
 public class ImageHandler {
  
@@ -24,16 +32,29 @@ public class ImageHandler {
    
     public int uploadImage(String description, String type, Part filePart) {
         try {
-            String sql = "INSERT INTO picture (description, type, image) VALUES (?, ?, ?)";
+            String sql = "INSERT INTO picture (description, type, image, thumbnail) VALUES (?, ?, ?, ?)";
             PreparedStatement prepared = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             InputStream inputStream = null;
+            InputStream inputThumb = null;
             if (filePart != null) {
                 inputStream = filePart.getInputStream();
+                inputThumb = filePart.getInputStream();
             }
             if (inputStream != null) {
                 prepared.setString(1, description);
                 prepared.setString(2, type);
+                //Thumbnail
                 prepared.setBlob(3, inputStream);
+                BufferedImage image = ImageIO.read(inputThumb);
+                BufferedImage thumbnail = resize(image, 50, 50);
+                JOptionPane.showMessageDialog(null, thumbnail.getHeight());
+                ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+                
+                // Hvor bliver  det skrevet?
+                ImageIO.write(thumbnail, type, byteOut);
+                InputStream is = new ByteArrayInputStream(byteOut.toByteArray());
+
+                prepared.setBlob(4, is);
                 prepared.executeUpdate();
                 ResultSet rs = prepared.getGeneratedKeys();
                 while (rs.next()) {
@@ -46,6 +67,18 @@ public class ImageHandler {
         return -1;
     }
     
+    public BufferedImage resize(BufferedImage img, int newW, int newH) { 
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
+    }  
+    
+        
     public int uploadMainImage(String description, String type, Part filePart, int buildingID, int fk_idMainPicture) {
         try {
             String sql = "DELETE FROM picture WHERE idPicture=?";
